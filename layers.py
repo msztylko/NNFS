e5083278e3b479c6a85405b10bd1a93820d9057b
Marcin Sztylko <marcin.sztylko@gmail.com>
2021-01-18 19:33:42 +0100

    update dense layers

--- layers.py
+++ layers.py
@@ -1,7 +1,10 @@
+import numpy as np
+
 class Layer_Dense:
 
     def __init__(self, n_inputs, n_neurons):
-        pass
+        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
 
     def forward(self, inputs):
-        pass
+        self.output = np.dot(inputs, self.weights) + self.biases

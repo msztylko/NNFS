67d2cb609cfa88af93448e916da8b71def2e9b68
Marcin Sztylko <marcin.sztylko@gmail.com>
2021-01-19 18:50:13 +0100

    modify softmax

--- layers.py
+++ layers.py
@@ -17,7 +17,7 @@ class Activation_ReLU:
 class Activation_Softmax:
     
     def forward(self, inputs):
-        exp_values = np.exp(inputs, axis=1, keepdims=True)
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
 
         probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
 

070b9c556fc282ea756d0d95ae98fbfa3b40454c
Marcin Sztylko <marcin.sztylko@gmail.com>
2021-01-19 18:48:30 +0100

    activation functions

--- layers.py
+++ layers.py
@@ -8,3 +8,17 @@ class Layer_Dense:
 
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
+
+class Activation_ReLU:
+    
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+class Activation_Softmax:
+    
+    def forward(self, inputs):
+        exp_values = np.exp(inputs, axis=1, keepdims=True)
+
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+
+        self.output = probabilities

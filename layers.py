5061f168037ed79cf519aaf9d6467b0c3e5735b3
Marcin Sztylko <marcin.sztylko@gmail.com>
2021-01-20 15:40:29 +0100

    categorical cross entropy loss

--- layers.py
+++ layers.py
@@ -33,3 +33,25 @@ class Loss:
         data_loss = np.mean(sample_losses)
 
         return data_loss
+
+class Loss_CategoricalCrossEntropy(Loss):
+    
+    def forward(self, y_pred, y_true):
+
+        samples = len(y_pred)
+
+        # Clip data to prevent division by 0
+        # Clip both sides to not drag mean towards any value
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+
+        # Probabilites for traget values -
+        # only if categorical labels
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[range(samples), y_true]
+
+        elif len(y_true.shape) == 2:
+            correct_confidences = np.sum(y_pred_clipped * y_true, axis=1)
+
+        # Losses
+        negative_log_likelihoods = -np.log(correct_confidences)
+        return negative_log_likelihoods

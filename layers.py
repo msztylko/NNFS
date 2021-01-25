18dc1a7dafcb9cb53a9779927bb537ab379a3be9
Marcin Sztylko <marcin.sztylko@gmail.com>
2021-01-25 11:33:37 +0100

    save inputs

--- layers.py
+++ layers.py
@@ -5,12 +5,13 @@ nnfs.init()
 
 class Layer_Dense:
 
-    def __init__(self, n_inputs, n_neurons):
-        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
-        self.biases = np.zeros((1, n_neurons))
+    def __init__(self, inputs, neurons):
+        self.weights = 0.01 * np.random.randn(inputs, neurons)
+        self.biases = np.zeros((1, neurons))
 
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
+        self.inputs = inputs
 
 class Activation_ReLU:
     

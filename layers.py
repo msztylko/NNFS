7e63afb901ccec1d089ee8db5992097b7ac6c7ad
Marcin Sztylko <marcin.sztylko@gmail.com>
2021-01-25 11:33:37 +0100

    backpropagation in Dense and ReLU layers

--- layers.py
+++ layers.py
@@ -13,10 +13,26 @@ class Layer_Dense:
         self.output = np.dot(inputs, self.weights) + self.biases
         self.inputs = inputs
 
+    def backward(self, dvalues):
+        # Gradients on parameters
+        self.dweights = np.dot(self.inputs.T, dvalues)
+        self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
+        # Gradient on values
+        self.dinputs = np.dot(dvalues, self.weights.T)
+
 class Activation_ReLU:
     
     def forward(self, inputs):
+        self.inputs = inputs
         self.output = np.maximum(0, inputs)
+
+    def backward(self, dvalues):
+        # Since we need to modify the original variable,
+        # let's make a copy of the values first
+        self.dinputs = dvalues.copy()
+
+        # Zero gradient where input values were negative
+        self.dinputs[self.inputs <= 0] = 0
 
 class Activation_Softmax:
     
